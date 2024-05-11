987df5db62247e9dfa459122f676baca1fe967b8
matteopolak <matteopolak@hotmail.com>
2024-05-11 19:42:06 -0400

    feat: add timeout in wasm executor

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,9 +1,19 @@
+use std::{sync::mpsc, thread, time};
+
 use quasi::{executor, lexer, parser, Error};
 use wasm_bindgen::prelude::wasm_bindgen;
 
 #[wasm_bindgen]
-pub fn execute(input: &[u8]) -> Result<String, String> {
-	wrap(input).map_err(|e| e.format(input))
+pub fn execute(input: &[u8], timeout_ms: u64) -> Result<String, String> {
+	let timeout = time::Duration::from_millis(timeout_ms);
+	let (tx, rx) = mpsc::channel();
+
+	thread::scope(move |s| {
+		s.spawn(move || s.spawn(move || tx.send(wrap(input).map_err(|e| e.format(input)))));
+	});
+
+	rx.recv_timeout(timeout)
+		.unwrap_or_else(|_| Err(format!("execution timed out after {}ms", timeout_ms)))
 }
 
 fn wrap(input: &[u8]) -> Result<String, Error> {
